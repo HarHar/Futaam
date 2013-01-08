012766f7864e452e311a6b9f96a74ae4c3e52e67
HarHar <harhar-captain@live.com>
2013-01-08 02:30:37 -0200

    Parser base

---
+++ parser.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import pickle
+import json
+import os
+from StringIO import StringIO
+
+class Parser(object):
+	def __init__(self, filename):
+		if os.path.exists(filename):
+			self.filename = filename
+			f = open(filename, 'r')
+			txt = f.read().replace('\r', '')
+			lines = txt.split('\n')
+			f.close()
+
+			if len(lines) == 0:
+				raise Exception('Empty file')
+
+			if lines[0].lower() == '[json]':
+				self.dbtype = 'json'
+				self.dictionary = json.load(StringIO(txt[len('[json]\n'):]))
+			elif lines[0].lower() == '[pickle]':
+				self.dbtype = 'pickle'
+				self.dictionary = pickle.loads(txt[len('[pickle]\n':)])
+			else:
+				raise Exception('Invalid database type')
+		else:
+			raise Exception('File does not exists')
+	def save(self):
+		f = open(self.filename, 'w')
+		f.write('[' + self.type + ']\n')
+
+		if self.type == 'pickle':
+			f.write(pickle.dumps(self.dictionary))
+		elif self.type == 'json':
+			f.write(json.dumps(self.dictionary))
