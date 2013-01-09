42e51e2d4118f81fe38663d417e480c0b0e069e6
HarHar <harhar-captain@live.com>
2013-01-09 00:23:38 -0200

    Fix identation

--- interfaces/web.py
+++ interfaces/web.py
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+
 def main(argv):
 	print('Web interface. Arguments: ')
-    print(repr(arg))
+	print(repr(arg))
