faa1217349a50817ce2689327e0b35c3dc9afa7f
TacticalGenius <abtalerico@gmail.com>
2013-01-09 16:56:24 -0600

    added MALWrapper to utils.py since we will probably want that eventually

--- interfaces/common/utils.py
+++ interfaces/common/utils.py
@@ -14,6 +14,20 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
+import urllib
+import urllib2
+import re
+from HTMLParser import HTMLParser
+from PIL import Image
+try:
+	from BeautifulSoup import BeautifulStoneSoup
+	from bs4 import BeautifulSoup
+	from bs4 import UnicodeDammit
+except:
+	print 'You must have the python BeautifulSoup module: install pip and execute "pip install beautifulsoup; pip install beautifulsoup4", as root.'
+	exit()
+
+	
 
 class colors():
     def __init__(self):
@@ -32,3 +46,135 @@ class colors():
         self.warning = ''
         self.fail = ''
         self.default = ''
+
+def remove_html_tags(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+def HTMLEntitiesToUnicode(text):
+    """Converts HTML entities to unicode.  For example '&amp;' becomes '&'."""
+    text = unicode(BeautifulStoneSoup(text, convertEntities=BeautifulStoneSoup.ALL_ENTITIES))
+    return text
+
+class MALWrapper(object):
+	@staticmethod
+	def search(name, stype):
+		if not stype in ['anime', 'manga']:
+			raise TypeError('second parameter must be either "anime" or "manga"') #I know, TypeError isn't meant for that, but meh
+
+		try:
+			n = unicodedata.normalize('NFKD', name).encode('ascii','ignore')
+		except:
+			n = name
+		params = urllib.urlencode({'q': n})
+		queryurl = 'http://mal-api.com/'+ stype + '/search?%s' % params
+		res = urllib2.urlopen(queryurl)
+		dict = json.load(res)
+		return dict
+	@staticmethod
+	def details(id, stype):
+		if not stype in ['anime', 'manga']:
+			raise TypeError('second parameter must be either "anime" or "manga"')
+		queryurl = 'http://mal-api.com/'+ stype +'/'+str(id)
+		res = urllib2.urlopen(queryurl)
+		dict = json.load(res)
+		return dict
+	@staticmethod
+	def getCharacterList(animeId, animeTitle, stype):
+		if not stype in ['anime', 'manga']:
+			raise TypeError('second parameter must be either "anime" or "manga"')
+
+		url = 'http://myanimelist.net/'+ stype +'/' + str(animeId) + '/' + animeTitle + '/characters'
+		c = urllib2.urlopen(url).read().replace("'+'", '').replace("' + '", '')
+		bs = BeautifulSoup(c)
+
+		i = 0
+		girls = []
+		for t in bs.findAll('td')[11:]:
+		        try:
+		                spl = t.getText().split('\n')
+		                if spl[1] in girls:
+		                	continue
+		                if spl[2].find('Main') != -1 or spl[2].find('Supporting') != -1:
+		                        girls.append(spl[1])
+		        except:
+		                pass
+		        i += 1
+
+		d = []
+		for b in girls:
+			if b.find(',') != -1:
+				s = b.split(',')
+				b = s[1] + ' ' + s[0]
+			d.append(b.lstrip().rstrip().strip('\n'))	        
+
+		ids = []
+		for l in bs.findAll('a'):
+			try:
+				if l.get('href').find('character') == -1: continue
+			except:
+				continue
+			for g in girls:
+				try:
+					if l.get('href').split('/')[4] in ids:
+						continue
+					if l.getText() == g:
+						ids.append(l.get('href').split('/')[4])
+				except:
+					continue
+
+		thumbnails = []
+		for img in bs.findAll('div'):
+			if img.get('class') != ['picSurround']: continue
+			try:
+				if img.find('a').get('href').find('character') == -1:
+					continue		
+					print repr(img.find('a').get('href')) + ' - ' + repr(img.find('a').find('img').get('src'))
+			except:
+				continue
+
+			for g in girls:
+				try:
+					if img.find('a').find('img').get('src').find('questionmark') == -1 and img.find('a').find('img').get('src') in thumbnails:
+						continue
+					if img.find('a').get('href').find(g.split(' ')[0]) != -1:
+						thumbnails.append(img.find('a').find('img').get('src'))
+						continue
+
+					if img.find('a').get('href').find(g.split(' ')[1]) != -1:
+						thumbnails.append(img.find('a').find('img').get('src'))
+				except:
+					continue		
+
+		return (d, ids, thumbnails)
+	@staticmethod
+	def getCharacterInfo(name, id, raw=False):
+		c = urllib2.urlopen('http://myanimelist.net/character/' + str(id))
+		d = c.read()
+
+		dammit = UnicodeDammit(d.replace("'+'", '').replace("' + '", ''))
+
+		summary = dammit.unicode_markup.split('<div class="normal_header" style="height: 15px;">' + name)[1]
+		summary = summary.split('</div>')[1]
+		summary = summary.split('<div')[0]
+
+		jpname = name + dammit.unicode_markup.split('<div class="normal_header" style="height: 15px;">' + name)[1].split('</div>')[0]
+
+		bs = BeautifulSoup(dammit.unicode_markup)
+
+		img = ''
+		for image in bs.findAll('img'):
+			try:
+				if image.get('alt').find(name) != -1:
+					img = image.get('src')
+					break
+			except: continue
+
+		if raw == False:
+			return (HTMLEntitiesToUnicode(remove_html_tags(jpname)), HTMLEntitiesToUnicode(remove_html_tags(summary)).rstrip('\n').lstrip('\n'), img)
+		else:
+			return (jpname, summary, img, name)
+
+	@staticmethod
+	def getEpisodeNumber(id):
+		return MALWrapper.queryAnime(id, 'anime')['episodes']
