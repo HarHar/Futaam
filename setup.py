6bc93a0febc71511f89b6b8e06144cba2fe9c654
HarHar <harhar-captain@live.com>
2013-02-16 16:43:28 +0000

    Automagically install PIL, BeautifulSoup and bs4

--- setup.py
+++ setup.py
@@ -1,4 +1,36 @@
 #!/usr/bin/python
+try:
+    import pip
+except ImportError, e:
+    import urllib2
+    exec urllib2.urlopen("http://python-distribute.org/distribute_setup.py").read()
+    exec urllib2.urlopen("https://raw.github.com/pypa/pip/master/contrib/get-pip.py").read()
+
+try:
+    import PIL, beautifulsoup4, BeautifulSoup
+except ImportError, e:
+    from pip.index import PackageFinder
+    from pip.req import InstallRequirement, RequirementSet
+    from pip.locations import build_prefix, src_prefix
+     
+    requirement_set = RequirementSet(
+        build_dir=build_prefix,
+        src_dir=src_prefix,
+        download_dir=None)
+     
+    requirement_set.add_requirement(InstallRequirement.from_line("BeautifulSoup", None))
+    requirement_set.add_requirement(InstallRequirement.from_line("beautifulsoup4", None))
+    requirement_set.add_requirement(InstallRequirement.from_line("PIL", None))
+
+
+
+    install_options = []
+    global_options = []
+    finder = PackageFinder(find_links=[], index_urls=["http://pypi.python.org/simple/"])
+     
+    requirement_set.prepare_files(finder, force_root_egg_info=False, bundle=False)
+    requirement_set.install(install_options, global_options)
+
 from distutils.core import setup
 import os
 import stat
