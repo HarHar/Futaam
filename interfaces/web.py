0c51443f20738a96caf7240b0653d3b83c9050a1
TacticalGenius <abtalerico@gmail.com>
2013-06-01 21:59:37 -0500

    sys is sys and os is os tfw

--- interfaces/web.py
+++ interfaces/web.py
@@ -15,8 +15,8 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os as sys # Because fuck you
-import sys as os # That's why.
+import os
+import sys
 import subprocess as sp
 
 def main(argv):
@@ -24,32 +24,32 @@ def main(argv):
 		sp.Popen(['git', '--version'], stdout=-1)
 	except:
 		print 'Git not found, please install it to continue'
-		os.exit(1)
+		sys.exit(1)
 
 	try:
 		sp.Popen(['node', '--version'], stdout=-1)
 	except:
 		print 'Node not found, please install it to continue'
-		os.exit(1)
+		sys.exit(1)
 
-	magicalDir = sys.path.dirname(sys.path.dirname(__file__))
+	magicalDir = os.path.dirname(sys.path.dirname(__file__))
 	alreadyInstalled = False
-	if sys.path.exists(magicalDir + '/Futaam-Web/'): alreadyInstalled = True
+	if os.path.exists(magicalDir + '/Futaam-Web/'): alreadyInstalled = True
 	if alreadyInstalled == False:
-		sys.popen('git clone git://github.com/that4chanwolf/Futaam-Web --depth=1 ' + magicalDir + '/Futaam-Web/')
-	if sys.path.exists(magicalDir + '/Futaam-Web'):
-		sys.chdir(magicalDir + '/Futaam-Web')
+		os.popen('git clone git://github.com/that4chanwolf/Futaam-Web --depth=1 ' + magicalDir + '/Futaam-Web/')
+	if os.path.exists(magicalDir + '/Futaam-Web'):
+		os.chdir(magicalDir + '/Futaam-Web')
 		try:
 			sp.Popen(['npm', '--version'], stdout=-1)
 		except:
 			print 'NPM not found, please install it to continue'
-			os.exit(1)
-		sys.popen('npm install') # Install dependencies
+			sys.exit(1)
+		os.popen('npm install') # Install dependencies
 		if alreadyInstalled == False:
 			print '\033[92mFutaam-Web is now installed. Use "node '+ magicaldir + '/Futaam-Web/FutaamWeb.js --db file" for launching it anytime\033[0m'
 		if len(argv) > 0:
 			print 'Launching Futaam-Web'
-			sys.system('node '+ magicalDir +'/Futaam-Web/FutaamWeb.js --db ' + argv[0])
+			os.system('node '+ magicalDir +'/Futaam-Web/FutaamWeb.js --db ' + argv[0])
 
 def help():
 	return 'Futaam-Web is maintened by that4chanwolf and can be found on https://github.com/that4chanwolf/Futaam-Web'
