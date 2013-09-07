57c115aa8a1260409adb33af6c9204c846b9e56c
HarHar <harhar-captain@live.com>
2013-09-06 22:20:11 -0300

    Modify setup.py to use setuptools+stuff for pypi and added console/gui entry points on futaam.py

--- setup.py
+++ setup.py
@@ -1,112 +1,14 @@
 #!/usr/bin/python
-try:
-    import pip
-except ImportError, e:
-    import urllib2
-    exec urllib2.urlopen("http://python-distribute.org/distribute_setup.py").read()
-    exec urllib2.urlopen("https://raw.github.com/pypa/pip/master/contrib/get-pip.py").read()
-
-try:
-    import PIL, beautifulsoup4, BeautifulSoup
-except ImportError, e:
-    from pip.index import PackageFinder
-    from pip.req import InstallRequirement, RequirementSet
-    from pip.locations import build_prefix, src_prefix
-     
-    requirement_set = RequirementSet(
-        build_dir=build_prefix,
-        src_dir=src_prefix,
-        download_dir=None)
-     
-    requirement_set.add_requirement(InstallRequirement.from_line("BeautifulSoup", None))
-    requirement_set.add_requirement(InstallRequirement.from_line("beautifulsoup4", None))
-    requirement_set.add_requirement(InstallRequirement.from_line("PIL", None))
-
-
-
-    install_options = []
-    global_options = []
-    finder = PackageFinder(find_links=[], index_urls=["http://pypi.python.org/simple/"])
-     
-    requirement_set.prepare_files(finder, force_root_egg_info=False, bundle=False)
-    requirement_set.install(install_options, global_options)
-
-from distutils.core import setup, Command
-from unittest import TextTestRunner, TestLoader
-from glob import glob
-from os.path import splitext, basename, join as pjoin, walk
-import os
-import sys
-import stat
-import platform
+#from distutils.core import setup
+from setuptools import setup
 
 PACKAGE = "futaam"
 NAME = "Futaam"
-DESCRIPTION = "An anime/manga list manager"
-AUTHOR = "HarHar"
-AUTHOR_EMAIL = "harhar-captain@live.com"
+DESCRIPTION = "An anime/manga/vn list (backlog) manager"
+AUTHOR = "HarHar/TacticalGenius"
+AUTHOR_EMAIL = "root@harh.net"
 URL = "https://github.com/HarHar/Futaam"
-VERSION = "0.1"
-
-
-class TestCommand(Command):
-	user_options = [ ]
-
-	def initialize_options(self):
-		self._dir = os.getcwd()
-
-	def finalize_options(self):
-		pass
-
-	def run(self):
-		testfiles = [ ]
-		for t in glob(pjoin(self._dir, 'tests', '*.py')):
-			if not t.endswith('__init__.py'):
-				testfiles.append('.'.join(
-				['tests', splitext(basename(t))[0]])
-			)
-
-		tests = TestLoader().loadTestsFromNames(testfiles)
-		t = TextTestRunner(verbosity = 1)
-		t.run(tests)
-		quit()
-
-def get_subpackages():
-	packs = []
-	os.chdir("interfaces")
-	for file in os.listdir(os.getcwd()):
-		if file[-2:] == "py":
-			packs.append("interfaces/" + file[:-3])
-	os.chdir("common")
-	for file in os.listdir(os.getcwd()):
-		if file[-2:] == "py":
-			packs.append("interfaces/common/" + file[:-3])
-	os.chdir("../..")
-	return packs
-
-SUBPACKAGES = get_subpackages()
-
-def get_ui_files():
-	files = []
-	os.chdir("interfaces/ui/")
-	for fileName in os.listdir(os.getcwd()):
-		if fileName[-2:] == "ui":
-			files.append("interfaces/ui/"+fileName)
-	os.chdir("../../")
-	return files
-
-UI_FILES = get_ui_files() 
-
-python_version = platform.python_version_tuple()
-python_version_string = str(python_version[0]) + "." + str(python_version[1])
-
-if os.name == "nt":
-	data_file_location = sys.prefix + "\\Scripts\\futaam"
-else:
-	if python_version[0] >= 2 and python_version[1] >= 6:
-		data_file_location = sys.prefix + "/local/share/futaam"
-	else:
-		data_file_location = sys.prefix + "/share/futaam"
+VERSION = __import__(PACKAGE).__version__
 
 setup(
     name=NAME,
@@ -116,30 +18,29 @@ setup(
     author_email=AUTHOR_EMAIL,
     license="GPL",
     url=URL,
-    py_modules=[PACKAGE] + SUBPACKAGES,
-    data_files=[(data_file_location, UI_FILES)],
-	classifiers=[
+    py_modules=[PACKAGE],
+    include_package_data=True,
+    classifiers=[
         "Development Status :: 5 - Production/Stable",
-        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Programming Language :: Python",
     ],
-	cmdclass = {"test": TestCommand}
+    install_requires=[
+       'BeautifulSoup >= 0.1.0',
+       'beautifulsoup4 >= 0.1.0',
+       'PIL >= 0.1.0',
+    ],
+    packages=['interfaces', 'tests', 'docs'],
+    package_data={'interfaces': ['common/*.py']},
+    entry_points={
+       'console_scripts': [
+          'futaam = futaam:main',
+       ],
+       'gui_scripts': [
+          'futaam-gui = futaam:maingui',
+       ],
+    },
 )
 
-if sys.argv[1] == "sdist":
-	sys.exit(0)
-
-if os.name == "nt":
-	print "Adding Futaam to your path"
-	path = sys.prefix + "\\Lib\\site-packages\\futaam.py"
-	os.popen("set PATH=%PATH%;" + path)
-else:
-	print "Putting a symlink to futaam.py in /usr/bin/"
-	try:
-		os.symlink("/usr/lib/python"+ python_version_string +"/site-packages/futaam.py", "/usr/bin/futaam")
-	except:
-		os.remove("/usr/bin/futaam")
-		os.symlink("/usr/lib/python"+ python_version_string +"/site-packages/futaam.py", "/usr/bin/futaam")
-		os.popen("chmod +x /usr/bin/futaam")
